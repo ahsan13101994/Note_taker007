4911580b6859afabed7dac6676f1287b03e5a6e8
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-06-18 15:58:29 -0400

    Get and Post function through ID

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,30 @@ module.exports = function(app){
     function addToDb(notes){
         notes = JSON.stringify(notes);
         fs.writeFileSync("./db/db.json", notes, function(err){
-            if (err) { return console.log(err); }
+            if (err) {return console.log(err);}
         });
     }
 
- // Write notes data to database
-        addToDb(notesList);
-
+// List of Notes
+    app.get("/api/notes", function(req, res){
+        res.json(notesList);
     });
+
+// Posting 
+    app.post("/api/notes", function(req, res){
+    //creating Notes ID
+        if (notesList.length == 0){
+            req.body.id = "0";
+
+        } else{
+            req.body.id = JSON.stringify(JSON.parse(notesList[notesList.length - 1].id) + 1);
+        }
+        
+        console.log("req.body.id: " + req.body.id);
+        notesList.push(req.body);
+        addToDb(notesList);
+        res.json(req.body);
+    });
+
+   
 };
